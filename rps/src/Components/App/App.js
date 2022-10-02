0bde2dac3c7fba429f97b43fa0737cee23e09402
Owen Ahmed <owenahmed1998@gmail.com>
2022-10-02 18:04:53 +0100

    MVP done.

---
+++ rps/src/Components/App/App.js
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import './App.css'
+
+// Rock = 0
+// Paper = 1
+// Scissors = 2
+
+function App() {
+  const [username, setUsername] = useState('');
+  const [cpu, setCpu] = useState();
+  const [cpuScore, setCpuScore] = useState(0);
+  const [userScore, setUserScore] = useState(0);
+  // const [moves, setMoves] = useState(0);
+
+  // if (moves === 0 && username === '') {
+  //   let name = window.prompt("What's your name?");
+  //   setUsername(name);
+  // }
+
+  function onChange(e){
+    setUsername(e.target.value)
+  }
+
+
+  function results(input) {
+    setCpu(Math.floor(Math.random() * 3));
+    if (cpu === 0) {
+      let move = "Computer chose rock.";
+      console.log(move);
+    } else if (cpu === 1) {
+      let move = "Computer chose paper.";
+      console.log(move);
+    } else if (cpu === 2) {
+      let move = "Computer chose scissors.";
+      console.log(move);
+    }
+
+    if (input === cpu) {
+      window.alert("Draw.");
+      // setMoves(moves + 1);
+    } else if (input - cpu === 1 || input - cpu === -2) {
+      window.alert(`${username} wins.`);
+      // setMoves(moves + 1);
+      setUserScore(userScore + 1);
+    } else {
+      window.alert(`${username} loses.`);
+      // setMoves(moves + 1);
+      setCpuScore(cpuScore + 1);
+    }
+  }
+
+  return (
+    <div className="App">
+      <h1>RPS DUEL</h1>
+      <p>Think you have what it takes?</p>
+      <input placeholder="Enter your name." onChange={onChange}></input>
+      <span className="score">{`YOU ${userScore}`}-{`${cpuScore} `}CPU</span>
+      <div className="buttons">
+      <button
+        onClick={function () {
+          console.log("You chose rock.");
+          results(0);
+        }}
+      >
+        Rock
+      </button>
+      <button
+        onClick={function () {
+          console.log("You chose paper.");
+          results(1);
+        }}
+      >
+        Paper
+      </button>
+      <button
+        onClick={function () {
+          console.log("You chose scissors.");
+          results(2);
+        }}
+      >
+        Scissors
+      </button>
+      <div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
